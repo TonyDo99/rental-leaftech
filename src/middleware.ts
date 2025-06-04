6b720ff3dac9b77a0fec9ccfe810213db92a453c
phat.do <dotanphat20@gmail.com>
2025-06-04 16:15:52 +0700

    feat: middleware enhance

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,29 +2,32 @@ import {
   clerkMiddleware,
   ClerkMiddlewareAuth,
   createRouteMatcher,
-} from "@clerk/nextjs/server";
-import { NextRequest, NextResponse } from "next/server";
+} from '@clerk/nextjs/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-const isProtectedRoute = createRouteMatcher(["/dashboard"]);
+const isProtectedRoute = createRouteMatcher(['/dashboard']);
 
 export default clerkMiddleware(
   async (auth: ClerkMiddlewareAuth, req: NextRequest) => {
     const { sessionClaims } = await auth();
 
     // Restrict admin routes to users with specific permissions
-    if (isProtectedRoute(req) && sessionClaims?.metadata.role !== "admin") {
+    if (isProtectedRoute(req) && sessionClaims?.metadata.role !== 'admin') {
       return NextResponse.json(
-        "Unauthorization! Your account dont have permission",
+        { error: "Unauthorized! Your account doesn't have permission" },
+        { status: 403 }
       );
     }
-  },
+
+    return NextResponse.next();
+  }
 );
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    "/(api|trpc)(.*)",
+    '/(api|trpc)(.*)',
   ],
 };
